240ea80624d88d7432eec4218af994c2265952f5
Komal Meena <capkomal2203@gmail.com>
2025-05-17 17:28:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-project/",
+  base: "/my-project/", // Must match your repo name exactly
   plugins: [react()],
 });
